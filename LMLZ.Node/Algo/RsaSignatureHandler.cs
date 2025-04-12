451511ed2b7a1e51235baa84fffda96db859575d
wlodzimierzyk <kowyako.yandex.ru>
2025-04-12 15:07:31 +0200

    little bit rework for RSA signer

--- LMLZ.Node/Algo/RsaSignatureHandler.cs
+++ LMLZ.Node/Algo/RsaSignatureHandler.cs
@@ -1,21 +1,28 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
+using LMLZ.Node.Entity;
 
 namespace LMLZ.Node.Algo;
 
 public static class RsaSignatureHandler
 {
-    public static byte[] SignData(byte[] data, byte[] privateKeyPkcs1)
+    public static string SignTransaction(Transaction t, byte[] privateKeyPkcs1)
     {
         using var rsa = RSA.Create();
         rsa.ImportRSAPrivateKey(privateKeyPkcs1, out _);
 
-        return rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        var data = Encoding.UTF8.GetBytes($"{t.Timestamp:o}|{t.Sender}|{t.Receiver}|{t.Amount}|{t.PublicKey}");
+
+        return Convert.ToHexString(rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
     }
 
-    public static bool IsSignatureValid(byte[] signature, byte[] publicKey, byte[] data)
+    public static bool IsSignatureValid(Transaction t, byte[] publicKey)
     {
         using var rsa = RSA.Create();
         rsa.ImportRSAPublicKey(publicKey, out _);
+
+        var data = Encoding.UTF8.GetBytes($"{t.Timestamp:o}|{t.Sender}|{t.Receiver}|{t.Amount}|{t.PublicKey}");
+        var signature = Convert.FromHexString(t.Signature);
 
         return rsa.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
     }
